cc51f3124ad4e030bd51fcec65a815e425c13d72
Hector Meraz Chavez <hector.meraz@jarsa.com>
2026-05-04 15:09:36 -0600

    [MIG] stock_picking_filter_product: Migration to 18.0

--- stock_picking_filter_product/__manifest__.py
+++ stock_picking_filter_product/__manifest__.py
@@ -1,13 +1,12 @@
 # Copyright 2020 Alan Ramos - Jarsa
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
 {
-    "name": "Stock picking filter proucts",
+    "name": "Stock picking filter products",
     "summary": "Show only available products based on their location",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Warehouse",
-    "website": "https://github.com/OCA/stock-logistics-workflow"
-    "13.0/stock_filter_product",
-    "author": "Jarsa, " "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/stock-logistics-workflow",
+    "author": "Jarsa, Odoo Community Association (OCA)",
     "license": "LGPL-3",
     "application": False,
     "installable": True,
